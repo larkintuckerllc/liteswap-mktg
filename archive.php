864b933998de016de164b2b49587dcc44bc3a02f
John  Tucker <john@larkintuckerllc.com>
2017-11-21 11:13:07 -0500

    Posts

--- archive.php
+++ archive.php
@@ -1,13 +1,16 @@
 
 <?php get_header(); ?>
 <div class="container container--background">
-  <h3><?php single_cat_title( 'Posts from ' ); ?></h3>
+  <h3 id="archive__root__title" ><?php single_cat_title( 'Posts from ' ); ?></h3>
   <?php while ( have_posts() ) : the_post(); ?>
-      <div>
+      <div class="archive__root__post">
         <?php if ( has_post_thumbnail() ): ?>
-        <div>
-          <img src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>" />
-        </div>
+          <div class="archive__root__post__image">
+            <img
+              src="<?php the_post_thumbnail_url( 'thumbnail' ); ?>"
+              class="archive__root__post__image__image"
+            />
+          </div>
         <?php endif ?>
         <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
         <p><i><?php the_time( 'F j, Y' ); ?></i></p>
--- single.php
+++ single.php
@@ -1,15 +1,19 @@
 
 <?php get_header(); ?>
 <?php while ( have_posts() ) : the_post(); ?>
-  <div class="container container--background">
+  <div>
     <?php if ( has_post_thumbnail() ): ?>
-      <div>
-        <img src="<?php the_post_thumbnail_url('large'); ?>" />
+      <div
+        id="single__root__image"
+        style="background-image: url('<?php the_post_thumbnail_url('large'); ?>');"
+      >
       </div>
     <?php endif ?>
-    <h3><?php the_title(); ?></h3>
-    <p><i><?php the_time('F j, Y'); ?></i></p>
-    <?php the_content(); ?>
+    <div class="container container--background">
+      <h3><?php the_title(); ?></h3>
+      <p><i><?php the_time('F j, Y'); ?></i></p>
+      <?php the_content(); ?>
+    </div>
   </div>
 <?php endwhile; ?>
 <?php get_footer(); ?>
